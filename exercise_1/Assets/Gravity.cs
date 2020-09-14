5d1a8698eb3cc92036f114e409759bac3b2ddfcb
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-14 18:19:55 +0200

    Added Gravity.cs

---
+++ exercise_1/Assets/Gravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    private const float GRAVITY = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //update the position
+        transform.position = transform.position + new Vector3(0, GRAVITY * Time.deltaTime, 0);
+
+        ////output to log the position change
+        //Debug.Log(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit:" + other.name);
+    }
+}
